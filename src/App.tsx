6dbf613cfde1e6a6514787e44312849b2b7d4a51
feli <felfeng@ucdavis.edu>
2025-02-08 23:22:48 -0800

    fixed header margin

--- src/App.tsx
+++ src/App.tsx
@@ -18,10 +18,10 @@ function App() {
         className="flex flex-row h-8 min-w-screen font-semibold text-white"
         style={{ backgroundColor: "#3A5F96" }}
       >
-        <div className="ml-2 w-4 py-2">
+        <div className="ml-2 w-4 py-2 ml-5">
           <img src={BrandyHeart} alt={BrandyHeart}></img>
         </div>
-        <div className="ml-6 py-1">feli's personal website</div>
+        <div className="ml-3 py-1">feli's personal website</div>
       </div>
       <div className="top-14 right-6 fixed items-center flex-col flex text-center">
         <img className="w-14" src={Resume}></img>
